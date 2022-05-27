4a479f8e0ee7ce7e694a2497affb44d9ef13a38d
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-27 21:59:15 +0530

    components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
 
 const API_KEY = process.env.REACT_APP_STREAM_API_KEY_SECRET;
 
+const client = StreamChat.getInstance(API_KEY);
+
 const App = () => {
   return (
-    <div>
-      <h1>Medical Pager</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChannelListContainer = () => {
+  return (
+    <div>
+      <h1>ChannelListContainer</h1>
+    </div>
+  );
+};
+
+export default ChannelListContainer;
