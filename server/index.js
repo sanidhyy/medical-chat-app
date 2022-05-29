7bb7717ad3b4a07d39a60c89063f137398107476
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-29 15:04:28 +0530

    nodejs server created

--- server/index.js
+++ server/index.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
