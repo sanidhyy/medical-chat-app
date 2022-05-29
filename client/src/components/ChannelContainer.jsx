cfc2fea73ee2b0c307d09b098bc8e1f1e85db95d
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-29 17:06:14 +0530

    chat ui exported from stream

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Channel, isChannel, useChatContext } from "stream-chat-react";
+import { Channel, useChatContext, MessageTeam } from "stream-chat-react";
 
-import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
+import { ChannelInner, CreateChannel, EditChannel } from "./";
 
 const ChannelContainer = ({
   isCreating,
@@ -43,7 +43,7 @@ const ChannelContainer = ({
     <div className="channel__container">
       <Channel
         EmptyStateIndicator={EmptyState}
-        Message={(messageProps, i) => <TeamMessage key={i} {...messageProps} />}
+        Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
       >
         <ChannelInner setIsEditing={setIsEditing} />
       </Channel>
