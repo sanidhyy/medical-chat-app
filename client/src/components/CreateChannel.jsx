121bd8203fb77b4cfb4bbcd2751f252a13bc6b37
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-30 17:07:31 +0530

    create channel functionality added

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -29,6 +29,26 @@ const CreateChannel = ({ createType, setIsCreating }) => {
   const [selectedUsers, setSelectedUsers] = useState([client.userID || ""]);
   const [channelName, setChannelName] = useState("");
 
+  const createChannel = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newChannel = await client.channel(createType, channelName, {
+        name: channelName,
+        members: selectedUsers,
+      });
+
+      await newChannel.watch();
+
+      setChannelName("");
+      setIsCreating(false);
+      setSelectedUsers([client.userID]);
+      setActiveChannel(newChannel);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="create-channel__container">
       <div className="create-channel__header">
@@ -46,6 +66,11 @@ const CreateChannel = ({ createType, setIsCreating }) => {
         />
       )}
       <UserList setSelectedUsers={setSelectedUsers} />
+      <div className="create-channel__button-wrapper" onClick={createChannel}>
+        <p>
+          {createType === "team" ? "Create Channel" : "Create Message Group"}
+        </p>
+      </div>
     </div>
   );
 };
