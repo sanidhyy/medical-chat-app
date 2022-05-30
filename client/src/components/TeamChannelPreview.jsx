9ccaf1c252a02ac43e378e5726aed0a0c92ed68d
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-30 22:49:40 +0530

    fixed direct messaging not showing issue

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -22,14 +22,16 @@ const TeamChannelPreview = ({
       ({ user }) => user.id !== client.userID
     );
 
+    console.log(members[0]);
+
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.fullName}
+          name={members[0]?.user?.fullName || members[0]?.user?.id}
           size={24}
         />
-        <p>{members[0]?.user?.fullName}</p>
+        <p>{members[0]?.user?.fullName || members[0]?.user?.id}</p>
       </div>
     );
   };
