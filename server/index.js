224497a44b0f2e2da4c26bf7dffd078f90146df8
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-05-31 22:55:05 +0530

    twilio sms notifications added

--- server/index.js
+++ server/index.js
@@ -8,12 +8,42 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
+const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const MESSAGING_SERVICE_SID = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
+});
+
+app.post("/", (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === "message.new") {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: MESSAGING_SERVICE_SID,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log("Message sent!"))
+            .catch((err) => console.log(err));
+        }
+      });
+
+    return res.send(200).send("Message sent!");
+  }
+
+  return res.status(200).send("Not a new message request!");
 });
 
 app.use("/auth", authRoutes);
